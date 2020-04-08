c4078338fe9a36666ccc7d598338948025b095c0
Ville <48829197+interwebzwizardi@users.noreply.github.com>
2020-04-08 16:35:46 +0300

    Update server.js

--- MongoDB-harjoitus/server.js
+++ MongoDB-harjoitus/server.js
@@ -52,7 +52,7 @@ app.post('/queryDB', (req,res)=>{
 });
 
 //Port config
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Listening port ${port}`);
 });
